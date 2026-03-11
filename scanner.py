b43de80b06b913999d7f1351692ceb009c186dfd
Ali Kalender <alikalender@Ali-Dizustu-Bilgisayar.local>
2026-03-11 17:49:11 +0200

    added a message if no open ports found

--- scanner.py
+++ scanner.py
@@ -1,46 +1,46 @@
 import socket
 import sys
 
-host = sys.argv[1]
-start_port = int(sys.argv[2])
-end_port = int(sys.argv[3])
-
 if len(sys.argv) != 4:
     print("Usage: python3 scanner.py <host> <start_port> <end_port>\n")
     sys.exit()
+
+host = sys.argv[1]
+start_port = int(sys.argv[2])
+end_port = int(sys.argv[3])
+ports_open = 0
 
 # Determine the common service for a port
 def get_service_name(port):
     try:
         service = socket.getservbyport(port)
         if service:
-            return service # Sometimes lookup returns "None", we convert it to "unknown" so program doesn't crash
+            return service
         else:
             return "unknown"
     except:
         return "unknown"
 
-# Read data sent by the service.
+# Read data sent by the service
 def grab_banner(host, port):
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # TCP connection over IPv4
-        s.settimeout(1) # Prevent socket from freezing by setting a timer 
-        s.connect((host, port)) # This opens a TCP connection to perform a handshake
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP over IPv4
+        s.settimeout(1)
+        s.connect((host, port))
 
         # For web servers, send a basic HTTP request
-
         if port == 80:
             s.send(b"HEAD / HTTP/1.0\r\nHost: " + host.encode() + b"\r\n\r\n")
-        
-        banner = s.recv(1024).decode(errors="ignore").strip() # Receive and decode up to 1024 bytes to string
-        s.close
 
-        if banner: # We only read the first line so we dont get a long information for cleaner output
+        banner = s.recv(1024).decode(errors="ignore").strip()
+        s.close()
+
+        if banner:
             first_line = banner.split("\n")[0]
             return first_line
         else:
             return "no banner"
-        
+
     except:
         return "no banner"
 
@@ -49,23 +49,18 @@ print(f"{'PORT':<9}{'STATE':<7}{'SERVICE':<10}BANNER")
 print("-" * 50)
 
 for port in range(start_port, end_port + 1):
-
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
 
     result = s.connect_ex((host, port))
 
     if result == 0:
+        ports_open += 1
         service = get_service_name(port)
         banner = grab_banner(host, port)
         print(f"{port}/tcp{'':<3}{'open':<7}{service:<10}{banner}")
-    
-    s.close
 
-        
-    
+    s.close()
 
-    
-
-
-
+if ports_open == 0:
+    print(f"No open ports between {start_port}-{end_port}")
